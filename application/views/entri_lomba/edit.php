82fcfc5affa23146b3ba817dac942d551d32647e
cedirusyaid <uttibatu@gmail.com>
2025-10-06 10:36:35 +0800

    fix: Resolve ParseError in entri_lomba/edit view

--- application/views/entri_lomba/edit.php
+++ application/views/entri_lomba/edit.php
@@ -8,7 +8,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="<?php echo base_url(); ?>">Home</a></li>
-              <li class="breadcrumb-item"><a href="<?php echo base_url('kompetisi'); ">Competitions</a></li>
+              <li class="breadcrumb-item"><a href="<?php echo base_url('kompetisi'); ?>">Competitions</a></li>
               <li class="breadcrumb-item"><a href="<?php echo base_url('kompetisi/view/' . $kompetisi['id']); ?>"><?php echo $kompetisi['nama']; ?></a></li>
               <li class="breadcrumb-item active">Edit Entry</li>
             </ol>
